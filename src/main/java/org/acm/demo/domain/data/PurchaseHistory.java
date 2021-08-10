e17fe44c18ef8c5eb22ee8b3ba814726a8c33242
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-08-10 22:54:49 +0430

    create purchase history class

---
+++ src/main/java/org/acm/demo/domain/data/PurchaseHistory.java
@@ -0,0 +1,49 @@
+package org.acm.demo.domain.data;
+
+import java.util.Date;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public class PurchaseHistory {
+    private Integer id;
+    private Cart cart;
+    private Date date;
+    private Long totalPrice;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public PurchaseHistory setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public PurchaseHistory setCart(Cart cart) {
+        this.cart = cart;
+        return this;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public PurchaseHistory setDate(Date date) {
+        this.date = date;
+        return this;
+    }
+
+    public Long getTotalPrice() {
+        return totalPrice;
+    }
+
+    public PurchaseHistory setTotalPrice(Long totalPrice) {
+        this.totalPrice = totalPrice;
+        return this;
+    }
+}
